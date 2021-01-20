d0619d8cf71040ba77852b22653193306a8532e9
Mateusz Boroch <m.boroch2@gmail.com>
2021-01-20 16:18:41 +0100

    Addconstructor in Data class

--- PersonalInformations/PersonalInformations/Data.cs
+++ PersonalInformations/PersonalInformations/Data.cs
@@ -9,8 +9,8 @@ namespace PersonalInformations
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string PESEL { get; set; }
-        public string Gender { get; }
-        public DateTime DateOfBirth { get; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
         public string Email { get; set; }
         public string NIP { get; set; }
 
@@ -18,6 +18,51 @@ namespace PersonalInformations
         public Data(string pesel)
         {
             //Set gender and date of birth from pesel number
+            string year,month,day;
+
+            if (pesel[2] ==2 || pesel[2] ==3)
+            {
+                year = "20";
+                year += pesel[0];
+                year += pesel[1];
+                if (pesel[2] == 2)
+                {
+                    month = "0";
+                    month += pesel[3];
+                }
+                else
+                {
+                    month = "1";
+                    month += pesel[3];
+                }
+                day = "";
+                day += pesel[4];
+                day += pesel[5];
+
+            }
+            else
+            {
+                year = "19";
+                year += pesel[0];
+                year += pesel[1];
+                month = "";
+                month += pesel[2];
+                month += pesel[3];
+                day = "";
+                day += pesel[4];
+                day += pesel[5];
+            }
+
+            DateOfBirth = new DateTime(int.Parse(year), int.Parse(month), int.Parse(day));
+            if (int.Parse(pesel[9].ToString())%2 == 1)
+            {
+                Gender = "Mężczyzna";
+            }
+            else
+            {
+                Gender = "Kobieta";
+            }
+
         }
 
     }
